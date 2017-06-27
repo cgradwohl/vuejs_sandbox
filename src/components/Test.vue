9665596b82ab2d9d4150191a89bc7f38eab9a66e
Chris Gradwohl <christophergradwohl@gmail.com>
2017-06-26 22:37:31 -0700

    cli scaffolding with webpack

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template id="">
+    <div class="test">
+        <h1>testing</h1>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'test'
+    }
+</script>
+
+<style scoped>
+
+</style>
